3344e67251a37e69d1270264eb340b3d62581860
Dmitrii Sosedov <dsosedov@vmware.com>
2021-05-06 09:37:12 -0400

    Prereq

---
+++ src/test/resources/k6/test.js
@@ -0,0 +1,16 @@
+// 1. init code
+
+export function setup() {
+  // 2. setup code
+
+}
+
+export default function (data) {
+  // 3. VU code
+
+}
+
+export function teardown(data) {
+  // 4. teardown code
+
+}
